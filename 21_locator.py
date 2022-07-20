2aaae237a1ef56ee2bb7998883ab57aefe4b116c
khabibullet <irek.rifson@gmail.com>
2022-07-20 18:41:04 +0300

    save token and uid into textfile

--- 21_locator.py
+++ 21_locator.py
@@ -2,18 +2,23 @@
 
 import socket
 import time
-# import sys
 import getpass
-
-# try:
-# 	import requests
-# except ImportError:
-# 	sys.exit()
+from slack_sdk import WebClient
 
 hostname = ' '.join(socket.gethostname().split(".")[:2])
 username = getpass.getuser()
 curr_time = time.strftime('%H:%M %Y-%m-%d', time.localtime())
 
 status = username + " last seen in " + hostname + " at " + curr_time
-print(status)
+status = "new status"
 
+json = {
+	"status_text": status,
+}
+with open('identity', 'r') as file:
+	slack_token, user_id = file.read().splitlines()
+	file.close()
+
+print(slack_token)
+# client = WebClient(token=slack_token)
+# response = client.users_profile_set(user=user_id, profile=json)
